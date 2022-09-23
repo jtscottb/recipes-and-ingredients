438d779a71f4466a16c069f8dc7551895070853d
Jacob <jacobmjones17@gmail.com>
2022-09-22 21:59:02 -0400

    I have added a lot more CSS styling from examples in bootstrap and bootstrap videos

--- client/src/components/LoginSignupContainer/LoginSignupContainer.js
+++ client/src/components/LoginSignupContainer/LoginSignupContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import "./LoginSignupContainer.css"
 import Login from '../login/Login';
 import Signup from '../signup/Signup';
@@ -6,12 +6,16 @@ import Signup from '../signup/Signup';
 const LoginSignupContainer = () => {
     const [login, setLogin] = useState(true);
     const [signup, setSignup] = useState(false)
+    const logininSignupContainerRef = useRef(null)
+    
     const handleClick = () => {
         setLogin(!login);
     }
 
+    logininSignupContainerRef.current.classList.toggle("active");
+
 return (
-    <div className="login-signup-container">
+    <div className="login-signup-container" ref={logininSignupContainerRef}>
     <Login />
     <div className="side-div">
     <button type="button" onClick={handleClick}> {login ? "Login" : "Signup"}</button>
--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -3,8 +3,15 @@ import "./Login.css"
 
 const Login = () => {
     return (
-        <div>Login</div>
-    )
+        <div className="login">
+            <h1>Login</h1>
+            <form>
+                <input type={"email"} placeholder={"Email"} />
+                <input type={"password"} placeholder={"Password"} />
+                <button type={"submit"}>Login</button>
+            </form>
+        </div>
+    );
 }
 
 export default Login
