20118bebaf2aa200bc9cfdea5ae2b300fee79557
Jacob <jacobmjones17@gmail.com>
2022-09-24 16:46:25 -0400

    I added methods inside of each of the classes inside of the models. I am about to set up my controller.

--- server/app/models/ingredient.rb
+++ server/app/models/ingredient.rb
@@ -1,3 +1,12 @@
 class Ingredient < ActiveRecord::Base
     belongs_to :recipe
+
+    def self.all_ingredients
+        self.all.order(name: :asc).pluck(:name)
+    end
+
+    def self.all_measurements
+        self.all.order(measurement: :asc).pluck(:measurement)
+    end
+    
 end
--- server/app/models/recipe.rb
+++ server/app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ActiveRecord::Base
     has_many :ingredients
+
+    def self.create_new(id, name)
+        self.create(id: id, name: name)
+    end
+
+    def delete(id)
+        self.find(id).destroy
+    end
 end
