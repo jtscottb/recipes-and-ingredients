f5809ce59c0727b94765b9295b89a0807d3be09e
Jacob <jacobmjones17@gmail.com>
2022-09-24 16:58:07 -0400

    I set up my CRUD requests in my backend controller.

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -2,23 +2,34 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
-  
-  get "/recipes" do
-    recipes = Recipe.all.order(:id)
-    recipes.to_json
-  end
-
-  get "/ingredients" do
-    ingredients = Ingredient.all.order(:id)
-    ingredients.to_json
+  post "/recipes" do
+    recipe = Recipe.create(
+      name: params[:name],
+      instructions: params[:instructions],
+      ingredient_id: params[:ingredient_id],
+      picture: params[:picture]
+      )
+      recipe.to_json
   end
 
-  #Add New Recipe from form
   get "/recipes" do
-    recipe = Recipe.create(recipe_name: params[recipe_name])
+    recipe = Recipe.all.order(:name)
+    recipe.to_json
+  end
+
+  patch "/recipes/:id" do
+    recipe = Recipe.find(params[:id])
+    recipe.update(
+      name: params[:name],
+      instructions: params[:instructions],
+      picture: params[:picture]
+    )
+    recipe.to_json
+  end
+
+  delete "/recipes/:id" do
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
     recipe.to_json
   end
 
